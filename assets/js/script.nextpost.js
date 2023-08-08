96c7ef4fdc9fa1499d5ee621208dc580548d2b6b
Amandamoonchild <amandasilva@aluno.senai.br>
2023-08-08 15:13:43 -0300

    feat: create show post function

--- assets/js/script.nextpost.js
+++ assets/js/script.nextpost.js
@@ -1,23 +1,24 @@
 const posts = [];
 
-function savePost(){
+function savePost() {
     const title = document.getElementById("title").value;
     const categories = document.getElementById("categories").value;
     const resume = document.getElementById("resume").value;
     const publisher = document.getElementById("publisher").value;
     const date = document.getElementById("date").value;
 
-   if (title && categories && resume && publisher && date){
-    storePost(title, categories, resume, publisher, date);
-    cleanFields();
-   } else {
-    alert("preencha todos os campos!");
-   }
+    if (title && categories && resume && publisher && date) {
+        storePost(title, categories, resume, publisher, date);
+        cleanFields();
+        showPosts() ;
+    } else {
+        alert("preencha todos os campos!");
+    }
 
-    
+
 }
 
-function cleanFields(){
+function cleanFields() {
     document.getElementById("title").value = "";
     document.getElementById("categories").value = "";
     document.getElementById("resume").value = ""
@@ -25,14 +26,35 @@ function cleanFields(){
     document.getElementById("date").value = "";
 }
 
-function storePost(title, categories, resume, publisher, date){
+function storePost(title, categories, resume, publisher, date) {
     const post = {
         title,
         categories,
-        resume ,
+        resume,
         publisher,
         date,
     };
     posts.push(post)
     console.log(post)
+}
+
+function showPosts() {
+    let showContent = "";
+
+    posts.forEach((post, index) => {
+        showContent += `
+        <div class="itemPost">
+            <h2>${post.title}</h2>
+           <p><strong> Categoria: </strong> ${post.categories}</p>
+           <p><strong> Resumo: </strong> ${post.resume}</p>
+           <p><strong> Autor: </strong> ${post.publisher}</p>
+           <p><strong> Data: </strong> ${post.date}</p>
+
+           <button onclick="editPost(${index})">Editar</button>
+           <button onclick="deletePost(${index})">Excluir</button>
+        </div>
+        `;
+    });
+
+    document.getElementById("list").innerHTML = showContent;
 }
